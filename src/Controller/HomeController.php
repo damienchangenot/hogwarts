b3baba2f04cb3e30b739b8429e6e28e7221f1012
damien <damien.changenot@gmail.com>
2024-06-13 16:54:33 +0200

    css page about

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -18,6 +18,15 @@ class HomeController extends AbstractController
     #[Route('/about', name: 'app_about')]
     public function about(): Response
     {
-        return $this->render('home/about.html.twig');
+        $celebrities = [
+            [ 'name' => 'Norbert Dragonneau' , 'picture' =>'dragonneau.jpg' , 'comment' => 'Un des plus célèbres magizoologiste, auteur du livre <i>Vie et habitat des animaux fantastiques<i/>' ],
+            [ 'name' => 'Harry Potter', 'comment' => 'Le nouveau né le plus célèbre au monde après le bébé royal...', 'picture' => 'hp1.jpg'],
+            [ 'name' => 'Albus Dumbledore', 'picture' => 'dumbledore.jpg', 'comment' => 'Le plus célèbre directeur de notre école ainsi qu\'un des sorciers les plus puissant de notre époque'],
+            [ 'name' => 'Gilderoy Lockhart', 'picture' => 'gilderoy.png', 'comment' => 'Eh mince on dirait que Peeves fait encore des siennes...'],
+            [ 'name' => 'Tom Jedusor', 'picture' => 'voldemort.png', 'comment' => 'Une créme solaire  indice 50 achetée, une offerte']
+        ];
+        return $this->render('home/about.html.twig', [
+            'celebrities' => $celebrities
+        ]);
     }
 }
